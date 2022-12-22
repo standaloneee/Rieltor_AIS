43f6ed569f138daa5a615412bfdb5ad1c528f206
standaloneee <tzherzdev@gmail.com>
2022-12-22 07:08:21 +0300

    personnel fix

--- Rieltor_AIS/Forms/Personnel.cs
+++ Rieltor_AIS/Forms/Personnel.cs
@@ -12,7 +12,7 @@ namespace Rieltor_AIS
             StartPosition = FormStartPosition.Manual;
             Location = new Point(250, 250);
             conn.Open();
-            adapter = new NpgsqlDataAdapter("SELECT * FROM \"users\"", conn);
+            adapter = new NpgsqlDataAdapter("SELECT * FROM \"personnel\"", conn);
             adapter.Fill(ds);
             dataGridView1.DataSource = ds.Tables[0];
             conn.Close();
